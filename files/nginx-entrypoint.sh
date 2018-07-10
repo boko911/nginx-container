5b8d15f3b9b878f65573f6e771729467ea5ea035
boko911 <nbuhler91@gmail.com>
2018-07-10 14:42:53 +0200

    Update nginx-entrypoint.sh

--- files/nginx-entrypoint.sh
+++ files/nginx-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ $DATABASE_NAME = "analyticsdashboard" ]; then
-  cp /static/dashboards/images/logos/favicon-prod.png /static/dashboards/images/logos/favicon.png 
+if [ $DEPLOYMENT = "prod" ]; then
+  cp /static/dashboards/images/logos/favicon-prod.png /static/dashboards/images/logos/favicon.png
 else
   cp /static/dashboards/images/logos/favicon-dev.png /static/dashboards/images/logos/favicon.png
 fi
